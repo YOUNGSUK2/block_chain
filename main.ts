41b40ff1e9b974e244c342ce103a3c0236743e7b
YOUNGSUK2 <36030527+YOUNGSUK2@users.noreply.github.com>
2018-02-12 20:12:02 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -68,4 +68,3 @@ console.log(tinycoin.chain[1].hash.toString());
 console.log(tinycoin.chain[2].previousHash.toString());
 console.log(tinycoin.chain[2].hash.toString());
 // console.log(genesis.createHash().toString());
-11KHKHKHL
